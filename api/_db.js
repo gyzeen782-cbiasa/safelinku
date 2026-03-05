026bb35a5d992e2d2447c1f6f73f73215f6f1c6d
gyzeen782-cbiasa <gyzeen782@gmail.com>
2026-03-05 13:24:18 +0700

    Update _db.js

--- api/_db.js
+++ api/_db.js
@@ -2,6 +2,7 @@
 const BIN_ID = process.env.JSONBIN_BIN_ID;
 const API_KEY = process.env.JSONBIN_API_KEY;
 const ADMIN_PW = process.env.ADMIN_PASSWORD || 'admin123';
+const MEMBER_KEY = process.env.MEMBER_KEY || 'member123';
 const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
 
 async function readDB() {
@@ -40,10 +41,15 @@ function checkAdmin(req) {
   return pw === ADMIN_PW;
 }
 
+function checkMember(req) {
+  const key = req.headers['x-member-key'];
+  return key === MEMBER_KEY || key === ADMIN_PW; // admin juga bisa pakai endpoint member
+}
+
 function cors(res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type,X-Admin-Password');
 }
 
-module.exports = { readDB, writeDB, genId, checkAdmin, cors, ADMIN_PW };
+module.exports = { readDB, writeDB, genId, checkAdmin, checkMember, cors, ADMIN_PW, MEMBER_KEY };
